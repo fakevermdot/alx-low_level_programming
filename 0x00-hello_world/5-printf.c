505a411634e5914e40cb3599dd1ae2d1ba4d291a
fakevermdot <dxgamergr@gmail.com>
2023-06-15 13:42:31 -0700

    alx C hello

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line with puts function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("\"with a proper grammer, but the outcome is a peice of art,\n");
+	return (0);
+}
+
