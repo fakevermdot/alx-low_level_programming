cda0ee854d845c7a0334e2868133631531ddd0a1
fakevermdot <dxgamergr@gmail.com>
2023-06-15 13:52:40 -0700

    alx c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A program that prints a line with puts function
+ * Return: 1 (Success)
+ */
+int main(void)
+{
+	fput("and that peice of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
+
